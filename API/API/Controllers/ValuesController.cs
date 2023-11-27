d09020c98488a7de5747ef6a2d897aa59b3d98b0
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2023-11-27 10:14:22 +0300

    Ajax_Post

--- API/API/Controllers/ValuesController.cs
+++ API/API/Controllers/ValuesController.cs
@@ -12,22 +12,50 @@ namespace API.Controllers
     {
         dbGoTripEntities1 db = new dbGoTripEntities1 ();
         // GET api/values
-        public IEnumerable<tblTurPaket> Get()
+        public IQueryable<tblTurPaket> Get()
         {
-            return db.tblTurPakets.ToList();
+            return db.tblTurPakets;
             //return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
-        public tblTurPaket Get(int id)
+        public HttpResponseMessage Get(int id)
         {
-            return db.tblTurPakets.FirstOrDefault(i => i.Id == id);
+            tblTurPaket paket = db.tblTurPakets.FirstOrDefault(i=>i.Id==id);
+            if (paket == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"Id'si {id} olan çalışan bulunamadı.");
+
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, paket);
+
             //return "value";
         }
 
         // POST api/values
-        public void Post([FromBody] string value)
+        public HttpResponseMessage Post(tblTurPaket tblTurPaket)
         {
+            try
+            {
+                db.tblTurPakets.Add(tblTurPaket);
+
+                if (db.SaveChanges()>0)
+                {
+                    HttpResponseMessage message = Request.CreateResponse(HttpStatusCode.Created, tblTurPaket);
+                    message.Headers.Location = new Uri(Request.RequestUri + "/" + tblTurPaket.Id);
+                    return message;
+
+
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Veri ekleme işlemi yapılamadı.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
         }
 
         // PUT api/values/5
